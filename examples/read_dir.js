444ff8993970d67a0fec56fba959dcc67fcfb074
Gabor Szabo <gabor@szabgab.com>
2015-02-01 09:11:53 +0200

    update main page

---
+++ examples/read_dir.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+
+
+if (process.argv.length <= 2) {
+    console.log("Usage: " + __filename + " path/to/directory");
+    process.exit(-1);
+}
+
+var path = process.argv[2];
+
+fs.readdir(path, function(err, items) {
+    console.log(items);
+
+    for (var i=0; i<items.length; i++) {
+        console.log(items[i]);
+    }
+});
+
