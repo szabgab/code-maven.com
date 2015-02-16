aa5c0b10c27d0b6e09e8ae55065914444ba95371
Gabor Szabo <gabor@szabgab.com>
2015-02-16 19:24:37 +0200

    swiss flag

--- examples/js/svg_flag_of_switzerland.js
+++ examples/js/svg_flag_of_switzerland.js
@@ -3,6 +3,7 @@ function flag_of_switzerland() {
     var width = 150;
     draw.size(width, width);
     var red   = draw.rect(width, width).fill({ color: '#FF0000' });
+
     var horizontal = draw.rect(2*width/3, width/5).dx(width/6).dy(2*width/5);
     horizontal.fill({ color: '#FFFFFF' });
     var vertical = draw.rect(width/5, 2*width/3).dx(2*width/5).dy(width/6);
