8ca7b60d08cea0aa2f20f461ccd05e2451c0c778
Gabor Szabo <gabor@szabgab.com>
2015-02-22 17:12:39 +0100

    ellipse

--- examples/js/svg_ellipse.js
+++ examples/js/svg_ellipse.js
@@ -1,12 +1,12 @@
 function ellipse() {
     var draw = SVG('ellipse_1');
-    draw.size(300, 200);
+    draw.size(200, 100);
     var ellipse = draw.ellipse(140, 70).attr({
         'fill'         : '#32AD4F',
         'stroke'       : '#723891',
         'stroke-width' : '3px'
     });
-    ellipe.dx(5).dy(5);
+    ellipse.dx(5).dy(5);
 }
 ellipse();
 
