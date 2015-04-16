bf8c80b0b15498f0b8b8f8acd2ec97aef3024635
Gabor Szabo <gabor@szabgab.com>
2015-04-16 16:05:11 +0300

    update pypi code

--- examples/js/pypi.js
+++ examples/js/pypi.js
@@ -1,22 +1,41 @@
-function show() {
-    var package = $('#name').val();
-    console.log(package);
-    var url = 'https://pypi.python.org/pypi/' + package + '/json';
-    console.log(url);
-    jQuery.get( url, '', function(data) {
+Handlebars.registerHelper('abc', function(options) {
+  //return options.fn(this);
+  return 'some text';
+});
+
+function show(package_name, package_version) {
+    //console.log(package_name);
+    var url = 'https://pypi.python.org/pypi/' + package_name;
+    if (package_version != undefined) {
+        url += '/' + package_version;
+    }
+    url += '/json';
+    //console.log(url);
+    jQuery.get( url, function(data) {
         console.log(JSON.stringify(data, null, 2));
 
         var source   = document.getElementById('text-template').innerHTML;
         var template = Handlebars.compile(source);
         var html    = template(data);
 
-        document.getElementById("text").innerHTML = html;
+        $("#text").html(html);
+        console.log(html);
 
+    }).fail(function() {
+        $("#text").html('Error');
     });
 }
 
 $(function() {
-    $('#show').click(show);
+    $('#show').click(function() {
+        show( $('#name').val() );
+    });
+    $('.pypi-link').click(function () {
+        var package_name    = $(this).attr('data-package');
+        var package_version = $(this).attr('data-version');
+        show(package_name, package_version);
+        event.stopPropagation();
+    });
 
 });
 
