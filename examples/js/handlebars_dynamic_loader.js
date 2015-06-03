ce2912e32958c281e8a56a942ee0fd230d9da4be
Gabor Szabo <gabor@szabgab.com>
2015-06-03 15:26:51 +0300

    handlebars article

---
+++ examples/js/handlebars_dynamic_loader.js
@@ -0,0 +1,31 @@
+var templates = {};
+
+function display_template(tmpl, data) {
+    console.log('display');
+    if (templates[tmpl] === undefined) {
+      return;
+    }
+
+    var template = templates[tmpl];
+    var html    = template(data);
+   $("#msg").html(html);
+}
+
+$(document).ready(function() {
+    $("#show").click(function () {
+      console.log('click');
+      var name = 'show';
+      var data = { time: new Date };
+
+       if (templates[name] === undefined) {
+         console.log("need");
+         jQuery.get("/try/examples/js/handlerbars_template_" + name + ".htm", function(resp) {
+             console.log(resp);
+             templates[name] = Handlebars.compile(resp);
+             display_template(name, data);
+         });
+       } else {
+          display_template(name, data);
+       }
+   });
+});
