3ee9028c33deffc8cad988c729523e8f76952608
Gabor Szabo <gabor@szabgab.com>
2015-06-04 19:50:06 +0300

    ==

--- examples/js/todo.js
+++ examples/js/todo.js
@@ -1,7 +1,7 @@
 function get_todos() {
     var todos = new Array;
     var todos_str = localStorage.getItem('todo');
-    if (todos_str != null) {
+    if (todos_str !== null) {
         todos = JSON.parse(todos_str); 
     }
     return todos;
