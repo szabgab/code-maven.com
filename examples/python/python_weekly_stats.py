b6fc2155867542e25cc6783cd876520bd47ca877
Gabor Szabo <gabor@szabgab.com>
2015-07-06 13:35:03 +0300

    python weekly

--- examples/python/python_weekly_stats.py
+++ examples/python/python_weekly_stats.py
@@ -1,10 +1,13 @@
 from __future__ import print_function
-import urllib
+import urllib, os, pickle
 from HTMLParser import HTMLParser
 
 
 
 class ArchiveHTMLParser(HTMLParser):
+    # def __init__():
+    #     pass
+
     def handle_starttag(self, tag, attrs):
         # <span class=date title="2015-04-12T08:11:49-05:00">
         if tag != 'a':
@@ -16,21 +19,37 @@ class ArchiveHTMLParser(HTMLParser):
         #     print(date)
 
 
+class Collector(object):
+    def __init__(self):
+        self.cache_file = 'python_weekly.pickle'
+        self.data = { 'issue': 0}
+        self.read_cache()
+
+    def read_cache(self):
+        if os.path.exists(self.cache_file):
+            fh = open(self.cache_file, 'r')
+            self.data = pickle.load(fh)
+            fh.close()
+    def save_cache(self):
+        print(self.cache_file)
+        fh = open(self.cache_file, 'w')
+        pickle.dump(self.data, fh)
+        fh.close()
+
+    def collect(self):
+        base_url = 'http://www.pythonweekly.com/archive/'
+        while (True):
+            self.data['issue'] += 1
+            url = base_url + str(self.data['issue']) + '.html'
+            print(url)
+            f = urllib.urlopen(url)
+            html = f.read()
+            f.close()
+            #print(html)
+            parser = ArchiveHTMLParser()
+            parser.feed(html)
+            break
+        self.save_cache()
 
 
-def collect():
-    base_url = 'http://www.pythonweekly.com/archive/'
-    i = 1
-    while (True):
-        url = base_url + str(i) + '.html'
-        print(url)
-        f = urllib.urlopen(url)
-        html = f.read()
-        f.close()
-        #print(html)
-        parser = ArchiveHTMLParser()
-        parser.feed(html)
-        break
-
-
-collect()
+Collector().collect()
