addb657850befe1b5c68563cc0153f838edeab69
Gabor Szabo <gabor@szabgab.com>
2015-08-02 17:15:16 +0300

    python weekly stats

--- examples/python/python_weekly_stats.py
+++ examples/python/python_weekly_stats.py
@@ -8,7 +8,6 @@ class ArchiveHTMLParser(HTMLParser):
         if tag != 'a':
             return
         attr = dict(attrs)
-#        print(attr)
         if 'href' in attr:
             self.urls.append(attr["href"])
         else:
@@ -27,7 +26,6 @@ class Collector(object):
             self.data = pickle.load(fh)
             fh.close()
     def save_cache(self):
-        #print(self.cache_file)
         fh = open(self.cache_file, 'w')
         pickle.dump(self.data, fh)
         fh.close()
@@ -37,46 +35,39 @@ class Collector(object):
         while (True):
             self.data['issue'] += 1
             url = base_url + str(self.data['issue']) + '.html'
-            #print(url)
             try:
                 f = urllib2.urlopen(url)
                 html = f.read()
-                #print(html)
                 f.close()
             except urllib2.HTTPError as e:
                 print(e, 'while fetching', url)
                 break
 
-            #print(html)
             parser = ArchiveHTMLParser()
             parser.urls = []
             parser.feed(html)
-            #print(parser.urls)
             for u in parser.urls:
                 if u == '#':
                     continue
-                #print(u)
                 if not re.search(r'^https?://', u):
                     print("Unhandled url: " + u)
+                    continue
                 match = re.search(r'^https?://([^/]+)', u)
                 if match:
-                    host = match.groups(1)[0]
+                    host = match.group(1)
                     if host not in self.data['hosts']:
                         self.data['hosts'][host]  = 0
                     self.data['hosts'][host] += 1
-                    #print(host)
-            #break
             self.save_cache()
 
     def report(self):
-        #print(self.data['hosts'])
         hosts, total = 0, 0
         for k in sorted(self.data['hosts'].keys(), key=lambda x: self.data['hosts'][x], reverse=True):
             hosts += 1
             total += int(self.data['hosts'][k])
             #print(k, self.data['hosts'][k])
             print('<li><a href="http://{0}/">{0}</a> {1}</li>'.format(k, self.data['hosts'][k]))
-        print("A total of {} links to {} hosts".format(total, hosts))
+        print("A total of {} links to {} hosts in {} issues.".format(total, hosts, self.data['issue']))
 
     def run(self):
         if len(sys.argv) == 2:
