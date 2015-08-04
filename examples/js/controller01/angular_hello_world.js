929813d12c551241cf0034574f3ac5f8925224fd
Gabor Szabo <gabor@szabgab.com>
2015-08-04 14:52:46 +0300

    remove old, unused examples

--- examples/js/controller01/angular_hello_world.js
+++
@@ -1,9 +0,0 @@
-function HelloWorldController() {
-    this.message = "Hello Angular World";
-};
-
-angular.module('hw', []);
-angular.module('hw').config(['$controllerProvider', function($controllerProvider) {
-  $controllerProvider.allowGlobals();
-}]);
-
--- examples/js/controller02/angular_hello_world.js
+++
@@ -1,9 +0,0 @@
-function HelloWorldController($scope) {
-    $scope.message = "Hello Angular World";
-};
-
-angular.module('hw', []);
-angular.module('hw').config(['$controllerProvider', function($controllerProvider) {
-  $controllerProvider.allowGlobals();
-}]);
-
