fc42d4d0ce74f6b35cb8f737fe33a42d7dcb89d7
Gabor Szabo <gabor@szabgab.com>
2015-10-12 12:17:33 +0300

    new article

---
+++ examples/ruby/data_structures.rb
@@ -0,0 +1,17 @@
+
+x = 43
+puts x.class      # Fixnum
+
+q = 3.14
+puts q.class      # Float
+
+z = "abc"
+puts z.class      # String
+
+colors = [ 'Blue', 'Gree', 'Yellow' ]
+puts colors.class # Array
+
+person = { 'fname' => 'Foo', 'lname' => 'Bar' }
+puts person.class  # Hash
+
+
