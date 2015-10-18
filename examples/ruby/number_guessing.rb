6255095e34e929888b8b450fc91ff62c54e97c31
Gabor Szabo <gabor@szabgab.com>
2015-10-18 11:32:36 +0300

    solution of number guesssing

---
+++ examples/ruby/number_guessing.rb
@@ -0,0 +1,17 @@
+
+hidden = rand(200)
+# puts hidden
+
+print "Type in your guess: "
+guess = gets.to_i
+
+if guess == hidden
+    puts "Hit"
+elsif guess < hidden
+    puts "Your guess is smaller that the hidden number"
+else
+    puts "Your guess is bigger that the hidden number"
+end
+
+
+
