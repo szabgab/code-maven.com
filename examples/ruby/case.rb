c99a51b992517ae77b2e32de7727538638a99ce3
Gabor Szabo <gabor@szabgab.com>
2015-10-29 19:33:02 +0200

    case array

---
+++ examples/ruby/case.rb
@@ -0,0 +1,14 @@
+
+name = 'Foo'
+puts name           # Ruby
+puts name.downcase  # ruby
+puts name.upcase    # RUBY
+puts name           # Ruby
+
+
+puts name.downcase! # ruby
+puts name           # ruby
+
+puts name.upcase!   # RUBY
+puts name           # RUBY
+
