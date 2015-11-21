565cf38f42dedddd47fceabc4950723621ec2968
Gabor Szabo <gabor@szabgab.com>
2015-11-21 08:17:16 +0200

    write to file in Ruby

---
+++ examples/ruby/append_to_file.rb
@@ -0,0 +1,4 @@
+f = File.new('out.txt', 'a')
+f.write("Hello World!\n")
+f.write("Hello Foo!\n")
+f.close     
---
+++ examples/ruby/write_to_file.rb
@@ -0,0 +1,4 @@
+f = File.new('out.txt', 'w')
+f.write("Hello World!\n")
+f.write("Hello Foo!\n")
+f.close     
