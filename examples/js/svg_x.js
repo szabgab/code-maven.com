e4d3e4abd188de01059d999b2f04684f64282fa5
Gabor Szabo <gabor@szabgab.com>
2015-12-24 22:33:28 +0200

    svg-x live

--- examples/js/svg_x.js
+++ examples/js/svg_x.js
@@ -1,10 +1,14 @@
 function draw_x() {
     var draw = SVG('draw_x');
     draw.size(200, 200);
-    draw.line(0, 0, 199, 199).attr({
+    draw.line(0, 0, 200,200).attr({
         'stroke'       : '#FF0000',
-        'stroke-width' : '3px'
+        'stroke-width' : '5px'
+    });
+    draw.line(0, 200, 200,0).attr({
+        'stroke'       : '#FF0000',
+        'stroke-width' : '5px'
     });
 }
 
-draw_line();
+draw_x();
