5b63a8de74d32814428c279ff497340e7bd4f65d
Gabor Szabo <gabor@szabgab.com>
2016-08-16 15:24:26 +0200

    commafy

---
+++ examples/python/commafy.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+
+small = 42
+big = 1234567
+
+print(small)      # 42
+print(big)        # 1234567
+
+print("{:,}".format(small)) # 42
+print("{:,}".format(big))   # 1,234,567
+
