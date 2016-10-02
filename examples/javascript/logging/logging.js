4841b016cdf884fe65f9535948ac82d3db34fcc6
Gabor Szabo <gabor@szabgab.com>
2016-10-02 07:11:48 +0300

    republish logging-in-javascript article

---
+++ examples/javascript/logging/logging.js
@@ -0,0 +1,9 @@
+function log_them() {
+    console.debug("Calling console.debug");
+    console.info("Calling console.info");
+    console.log("Calling console.log");
+    console.warn("Calling console.warn");
+    console.error("Calling console.error");
+}
+
+log_them();
