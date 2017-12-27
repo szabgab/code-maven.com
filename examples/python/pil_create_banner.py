a7bacd2d1f20e846109503a1de26af1a713d709d
Gabor Szabo <gabor@szabgab.com>
2017-12-27 11:54:55 +0100

    banner

---
+++ examples/python/pil_create_banner.py
@@ -0,0 +1,9 @@
+from PIL import Image, ImageDraw, ImageFont
+
+img = Image.new('RGB', (460, 60), color = (73, 109, 137))
+
+fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf', 20)
+d = ImageDraw.Draw(img)
+d.text((20 ,10), "Would you like to support the Code Maven site?", font=fnt, fill=(255, 255, 0))
+
+img.save('support-code-maven.png')
