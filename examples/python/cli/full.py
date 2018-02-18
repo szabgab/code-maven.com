a73dc0c87a7a8671dfd41bd7a56cc9f61e780ffb
Gabor Szabo <gabor@szabgab.com>
2018-02-18 13:07:47 +0200

    update full example

--- examples/python/cli/full.py
+++ examples/python/cli/full.py
@@ -5,15 +5,17 @@ class MyPrompt(Cmd):
     intro = "Welcome! Type ? to list commands"
 
     def do_exit(self, inp):
-        '''exit the application. Shorthand: x q.'''
         print("Bye")
         return True
+    
+    def help_exit(self):
+        print('exit the application. Shorthand: x q Ctrl-D.')
 
     def do_add(self, inp):
         print("adding '{}'".format(inp))
 
     def help_add(self):
-       print("Add a new entry to the system.")
+        print("Add a new entry to the system.")
 
     def default(self, inp):
         if inp == 'x' or inp == 'q':
@@ -21,4 +23,8 @@ class MyPrompt(Cmd):
 
         print("Default: {}".format(inp))
 
-MyPrompt().cmdloop()
+    do_EOF = do_exit
+    help_EOF = help_exit
+
+if __name__ == '__main__':
+    MyPrompt().cmdloop()
