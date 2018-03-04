9d50cad5068a081ad0ecf6a052f4f451306eb25b
Gabor Szabo <gabor@szabgab.com>
2018-03-04 08:17:46 +0200

    update imgae creation

--- examples/python/pil_create_banner.py
+++ examples/python/pil_create_banner.py
@@ -6,4 +6,6 @@ fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf', 20)
 d = ImageDraw.Draw(img)
 d.text((20 ,10), "Would you like to support the Code Maven site?", font=fnt, fill=(255, 255, 0))
 
+d.line((0, 0) + img.size, fill=128)
+
 img.save('support-code-maven.png')
