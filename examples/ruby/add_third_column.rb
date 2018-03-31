c36d79a2382af9a8e752f8081a7d36792c5240c9
Gabor Szabo <gabor@szabgab.com>
2018-04-01 00:34:52 +0300

    relative directory

--- examples/ruby/add_third_column.rb
+++ examples/ruby/add_third_column.rb
@@ -1,6 +1,7 @@
 require "CSV"
 
-filename = 'data.csv'
+
+filename = File.dirname(File.dirname(File.expand_path(__FILE__))) + '/data/distance.csv'
 sum = 0
 CSV.foreach(filename) do |row|
   sum += row[2].to_i
