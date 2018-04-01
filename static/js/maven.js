39b09db87099493f25754709c6628a4e985e3715
Gabor Szabo <gabor@szabgab.com>
2018-04-01 09:25:56 +0300

    experiment for top_poster

--- static/js/maven.js
+++ static/js/maven.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
     console.log("hello");
+    $('#top-poster').html('Welcome to the Code-Maven site! Check out th <a href="/">main page</a> for article series.');
+    $('#top-poster').show();
+    //$('#top-poster').css('display', 'block');
 })
