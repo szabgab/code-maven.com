8b9e88a43e95cd3bc04533a27608be031003fa84
Gabor Szabo <gabor@szabgab.com>
2018-04-01 09:51:12 +0300

    blueish

--- static/js/maven.js
+++ static/js/maven.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    console.log("hello");
     $('#top-poster').html('Welcome to the Code-Maven site! Check out the <a href="/">main page</a> for article series.');
+    $('#top-poster').css('font-size', '30px');
+    $('#top-poster').css('background-color', '#42e8f4');
     $('#top-poster').show();
-    //$('#top-poster').css('display', 'block');
 })
