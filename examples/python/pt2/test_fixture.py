36ed471ecccff09172403558c76be4278ecf2758
Gabor Szabo <gabor@szabgab.com>
2018-04-09 23:01:19 +0300

    republish python testing article

---
+++ examples/python/pt2/test_fixture.py
@@ -0,0 +1,19 @@
+
+def setup_function():
+    print("\nsetup_function")
+
+def teardown_function():
+    print("\nteardown_function")
+
+def test_one():
+    assert True
+    print("test_one")
+
+def test_two():
+    assert True
+    print("test_two")
+
+def test_three():
+    assert True
+    print("test_three")
+
---
+++ examples/python/pt2/test_some.py
@@ -0,0 +1,6 @@
+import sys
+
+def test_hello():
+    print("hello testing")
+    sys.stderr.write("stderr during testing\n")
+
