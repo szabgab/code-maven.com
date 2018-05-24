42b967f619ad8e31ac5d73230c364800ccdf739c
Gabor Szabo <gabor@szabgab.com>
2018-05-24 09:44:47 +0300

    groovy page

---
+++ examples/groovy/input_from_console.groovy
@@ -0,0 +1,3 @@
+def name = System.console().readLine 'What is your name? '
+println "Your name is " + name
+
---
+++ examples/groovy/input_from_stdin.groovy
@@ -0,0 +1,4 @@
+print "What is your name? "
+def name = System.in.newReader().readLine()
+println "Your name is " + name
+
