83ec2b4a2ec60b9590cbc176a969869ee2915b80
Gabor Szabo <gabor@szabgab.com>
2018-05-27 07:34:49 +0300

    updates

--- examples/python/svg_rectangle.py
+++ examples/python/svg_rectangle.py
@@ -5,12 +5,3 @@ dwg.add( dwg.rect(insert=(200, 100), size=(200, 100), fill='blue', stroke='red',
 with open("rect.svg", "w") as fh:
     fh.write(dwg.tostring())
 
-#import cairosvg
-#cairosvg.svg2png(bytestring=dwg.tostring(), write_to="rect.png")
-#cairosvg.svg2pdf(url="rect.svg", write_to="rect.pdf")
-
-
-#print(dwg.tostring())
-#print(dir(dwg))
-#dwg.save()
-
