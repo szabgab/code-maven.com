ede420f34b4708f01be1c5f126ea30636483b6d5
Gabor Szabo <gabor@szabgab.com>
2018-05-27 09:23:26 +0300

    more groovy

--- examples/groovy/number_guessing_game.groovy
+++ examples/groovy/number_guessing_game.groovy
@@ -1,6 +1,17 @@
 Random random = new Random()
-def hidden = random.nextInt(200)
+def hidden = random.nextInt(20)
 
-print "First number: "
-def x = System.in.newReader().readLine() as Integer
+while (true) {
+    print "Guess number: "
+    def guess = System.in.newReader().readLine() as Integer
+    if (guess < hidden) {
+        println "Guess was too small"
+    } else if (guess > hidden) {
+        println "Guess was too hight"
+    } else {
+        println "Match!"
+        break
+    }
+}
 
+println "Bye"
---
+++ examples/groovy/undeclared_variable.groovy
@@ -0,0 +1,10 @@
+print "Input: "
+def val = System.in.newReader().readLine() as Integer
+
+if (val > 10) {
+   println "bigger than 10"
+} else if (value < 10) {
+   println "smaller than 10"
+} else {
+   println "it is 10"
+}
