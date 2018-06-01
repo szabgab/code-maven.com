66289d5dffb921e6df1874a25f1d73dbe4e6b274
Gabor Szabo <gabor@szabgab.com>
2018-06-01 08:55:02 +0300

    groovy

--- examples/groovy/check_type.groovy
+++ examples/groovy/check_type.groovy
@@ -2,10 +2,8 @@ def obj = ["question", 42, 'answer']
 
 println obj.getClass()   // class java.util.ArrayList
 
-// Check if obj is the subclass of some class
 println (obj instanceof List)                   // true
 println (obj instanceof java.util.ArrayList)    // true
 
-// Check the exact type of an object
 println (obj.getClass() == List)                // false
 println (obj.getClass() == java.util.ArrayList) // true
