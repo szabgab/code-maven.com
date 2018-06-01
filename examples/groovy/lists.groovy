b07f6792a0b96e93b47d00074994cd113a4dd685
Gabor Szabo <gabor@szabgab.com>
2018-06-01 07:52:44 +0300

    new groovy page

---
+++ examples/groovy/lists.groovy
@@ -0,0 +1,7 @@
+
+def a_mess = ["question", 42, 'answer']
+println a_mess              // [question, 42, answer]
+println a_mess.size()       // 3
+println a_mess[0]           // question
+println a_mess.getClass()   // class java.util.ArrayList
+
---
+++ examples/groovy/sum_of_numbers.groovy
@@ -0,0 +1,10 @@
+sum = 0
+
+def 
+fh = new File('examples/data/numbers.txt')
+fh.eachLine( { println x } )
+
+println "The sum is: " + sum
+
+
+# http://groovy-lang.org/syntax.html#_lists
