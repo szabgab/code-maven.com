50bbee5bf8f1c745c76c49fb2b932adb2ea70020
Gabor Szabo <gabor@szabgab.com>
2018-06-19 17:27:52 +0300

    stop the ads

--- static/js/maven.js
+++ static/js/maven.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-    var msg = 'Welcome to the Code-Maven site! Check out the <a href="/">main page</a> for article series.';
-
-    $('#top-poster').html(msg);
-    $('#top-poster').css('font-size', '30px');
-    $('#top-poster').css('background-color', '#42e8f4');
-    $('#top-poster').show();
-
-    // $('#after-abstract').html(msg);
-
-    msg = 'Was this article useful? Support me via <a href="https://www.patreon.com/szabgab">Patreon</a>!';
-    $('#after-content').html(msg);
-    $('#after-content').css('font-size', '30px');
-    $('#after-content').css('background-color', 'orange');
-    $('#after-content').show();
+//    var msg = 'Welcome to the Code-Maven site! Check out the <a href="/">main page</a> for article series.';
+//
+//    $('#top-poster').html(msg);
+//    $('#top-poster').css('font-size', '30px');
+//    $('#top-poster').css('background-color', '#42e8f4');
+//    $('#top-poster').show();
+//
+//    // $('#after-abstract').html(msg);
+//
+//    msg = 'Was this article useful? Support me via <a href="https://www.patreon.com/szabgab">Patreon</a>!';
+//    $('#after-content').html(msg);
+//    $('#after-content').css('font-size', '30px');
+//    $('#after-content').css('background-color', 'orange');
+//    $('#after-content').show();
 })
