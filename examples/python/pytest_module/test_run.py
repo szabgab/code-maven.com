7efe438be86a6bf9441fd8137ccf1ae8cb4c7633
Gabor Szabo <gabor@szabgab.com>
2018-07-01 17:13:54 +0300

    sample pytest code

---
+++ examples/python/pytest_module/test_run.py
@@ -0,0 +1,10 @@
+def setup_module(module):
+    print(dir(module))
+
+def teardown_module(module):
+    print(dir(module))
+
+def test_run():
+    print("Runnin the test_run function")
+    assert True
+#    assert False
