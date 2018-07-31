8da96e63e5a48c5caaf43517dd23e9072d9d6089
Gabor Szabo <gabor@szabgab.com>
2018-07-31 22:59:44 +0300

    new groovy article

---
+++ examples/groovy/elements_of_map.groovy
@@ -0,0 +1,9 @@
+def colors = [red: '#FF0000', green: '#00FF00', blue: '#0000FF']
+println colors
+println colors.containsKey('red')
+println colors.containsKey('black')
+
+for (p in colors) {
+   println p.key
+   println p.value
+}
