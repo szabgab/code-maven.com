7ef306f350819fc9717350a4bbc647dfc5581e5d
Gabor Szabo <gabor@szabgab.com>
2018-08-02 09:13:39 +0300

    groovy json

---
+++ examples/groovy/reading_json.groovy
@@ -0,0 +1,14 @@
+import groovy.json.JsonSlurper
+json_str = '''{
+   "name": "Foo Bar",
+   "year": 2018,
+   "timestamp": "2018-03-08T00:00:00",
+   "tags": [ "person", "employee" ],
+   "grade": 3.14 }'''
+
+
+def jsonSlurper = new JsonSlurper()
+cfg = jsonSlurper.parseText(json_str)
+println cfg          // [name:Foo Bar, year:2018, timestamp:2018-03-08T00:00:00, tags:[person, employee], grade:3.14]
+println cfg['name']  // Foo Bar
+println cfg.name     // Foo Bar
