3f8f74c0734cb18cc982e348f7165e4e872e5455
Gabor Szabo <gabor@szabgab.com>
2019-04-20 09:49:38 +0300

    example

---
+++ examples/groovy/printf_width.groovy
@@ -0,0 +1,8 @@
+
+def v = 65
+printf("<%s>\n", v);     // <65>
+printf("<%10s>\n", v);   // <      65>
+printf("<%-10s>\n", v);  // <65      >
+printf("<%c>\n", v);     // <A>
+printf("<%d>\n", v);     // <65>
+printf("<%05d>\n", v);   // <00065>
