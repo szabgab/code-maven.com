5706da1488912c502900b483c66bf16ce5d51ee7
Gabor Szabo <gabor@szabgab.com>
2020-01-17 14:59:52 +0200

    python wc

--- examples/python/wc.py
+++ examples/python/wc.py
@@ -26,9 +26,17 @@ if __name__ == '__main__':
     if len(sys.argv) < 1:
         exit("Usage: {} FILENAMEs".format(sys.argv[0]))
     results = wc(*sys.argv[1:])
+    totals = {
+        'lines': 0,
+        'words': 0,
+        'chars': 0,
+    }
     for filename in results:
         res = results[filename]
         print("{} {} {} {}".format(res['lines'], res['words'], res['chars'], filename))
+        for k in res:
+            totals[k] += res[k]
+    print("{} {} {} {}".format(totals['lines'], totals['words'], totals['chars'], 'total'))
 
 
 # from wc import wc
@@ -40,15 +48,42 @@ def test_wc(tmpdir):
     temp_dir = str(tmpdir)
     print(temp_dir)
 
-    filename = os.path.join(temp_dir, 'a.txt')
-    with open(filename, 'w') as fh:
+    file_a = os.path.join(temp_dir, 'a.txt')
+    with open(file_a, 'w') as fh:
         fh.write("Hello World\n")
         fh.write("Second line\n")
 
-    assert wc(filename) == {
-        filename : {
+    file_b = os.path.join(temp_dir, 'b.txt')
+    with open(file_b, 'w') as fh:
+        fh.write("More than one    space\n")
+        fh.write("Special - characters!\n")
+        fh.write("No trailing newline")
+
+    assert wc(file_a) == {
+        file_a : {
             'lines': 2,
             'words': 4,
             'chars': 24,
-        }
+        },
+    }
+
+    assert wc(file_b) == {
+        file_b : {
+            'lines': 3,
+            'words': 10,
+            'chars': 64,
+        },
+    }
+
+    assert wc(file_a, file_b) == {
+        file_a : {
+            'lines': 2,
+            'words': 4,
+            'chars': 24,
+        },
+        file_b : {
+            'lines': 3,
+            'words': 10,
+            'chars': 64,
+        },
     }
