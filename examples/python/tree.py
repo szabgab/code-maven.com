e2eade03de2fedbbb3e8c1408943531e99d1ee46
Gabor Szabo <gabor@szabgab.com>
2020-03-12 07:44:16 +0200

    Publish python travers directory tree

---
+++ examples/python/tree.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+if len(sys.argv) != 2:
+    exit("Usage: {} PATH_TO_DIRECTORY".format(sys.argv[0]))
+
+root = sys.argv[1]
+
+for dirname, dirs, files in os.walk(root):
+    #print(dirname)     # relative path (from cwd) to the directory being processed
+    #print(dirs)       # list of subdirectories in the currently processed directory
+    #print(files)       # list of files in the currently processed directory
+    for filename in files:
+        print(os.path.join(dirname, filename))   # relative path to the "current" file
+
+    dirs[:] = [d for d in dirs if d != '.git']
+    #dirs[:] = [d for d in dirs if not d.startswith('.') and not d.startswith('__') and d not in ['tmp', 'logs']]
