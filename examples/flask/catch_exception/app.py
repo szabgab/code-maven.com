68681a09424abfb33e82702479e04edac5eaebef
Gabor Szabo <gabor@szabgab.com>
2020-06-26 11:08:33 +0300

    flask exception

---
+++ examples/flask/catch_exception/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, abort
+import datetime
+
+app = Flask(__name__)
+
+@app.errorhandler(Exception)
+def server_error(err):
+    app.logger.exception(err)
+    return "exception", 500
+
+@app.route("/")
+def main():
+    app.logger.info("main route")
+    return "Hello " + str(datetime.datetime.now())
+
+@app.route("/crash")
+def crash():
+    app.logger.info("crash route")
+    a = 0
+    b = 3 / a
