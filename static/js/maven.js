c7c90bfb2dc956f11b9a609d65cfc78e3d2a56cb
Gabor Szabo <gabor@szabgab.com>
2020-07-22 12:00:23 +0300

    message

--- static/js/maven.js
+++ static/js/maven.js
@@ -1,16 +1,24 @@
 $(document).ready(function() {
-//    var msg = 'Welcome to the Code-Maven site! Check out the <a href="/">main page</a> for article series.';
-//
-//    $('#top-poster').html(msg);
-//    $('#top-poster').css('font-size', '30px');
-//    $('#top-poster').css('background-color', '#42e8f4');
-//    $('#top-poster').show();
-//
-//    // $('#after-abstract').html(msg);
-//
+    var show_top = function(msg) {
+        $('#top-poster').html(msg);
+        $('#top-poster').css('font-size', '30px');
+        $('#top-poster').css('background-color', '#42e8f4');
+        $('#top-poster').show();
+    };
+    var show_bottom = function(msg) {
+        $('#after-content').html(msg);
+        $('#after-content').css('font-size', '30px');
+        $('#after-content').css('background-color', 'orange');
+        $('#after-content').show();
+    };
+
+    // $('#after-abstract').html(msg);
+
+    var path = window.location.pathname;
+    var re = new RegExp('docker|jenkins');
+    if (re.exec(path)) {
+        show_top('When you are done <a href="/devops-invitation">check out</a> how else we might help you.';
+    }
+
 //    msg = 'Was this article useful? Support me via <a href="https://www.patreon.com/szabgab">Patreon</a>!';
-//    $('#after-content').html(msg);
-//    $('#after-content').css('font-size', '30px');
-//    $('#after-content').css('background-color', 'orange');
-//    $('#after-content').show();
 })
