7408bf4c7bc8cccb07bb93cb483db7e48d77c343
Gabor Szabo <gabor@szabgab.com>
2020-10-03 15:56:57 +0300

    groovy

---
+++ examples/groovy/unicode.groovy
@@ -0,0 +1,12 @@
+def latin_letters = "Hello World"
+println latin_letters
+
+def hungarian = "Álom ló és ő egy ügyes ökör."
+println hungarian
+
+def hebrew = "שלום עולם"
+println hebrew
+
+def arabic = "السلام عليكم"
+println arabic
+
