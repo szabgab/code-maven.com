262dc67465340ab8290a593fa58514e1a587a2d1
Gabor Szabo <gabor@szabgab.com>
2021-06-21 12:48:11 +0300

    publish draft

---
+++ examples/ruby/rectangle_stdin.rb
@@ -0,0 +1,10 @@
+print "Width:"
+width = gets.strip.to_f
+
+print "Length:"
+length = gets.strip.to_f
+
+area = width * length
+circumference = 2 * (width + length)
+puts "area: #{area}"
+puts "circumference: #{circumference}"
