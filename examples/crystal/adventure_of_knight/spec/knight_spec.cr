74bf95273b3b5a48502f4c26c9d0d3ed2b9f8de1
Gabor Szabo <gabor@szabgab.com>
2021-06-27 19:43:38 +0300

    start working on the knight

---
+++ examples/crystal/adventure_of_knight/spec/knight_spec.cr
@@ -0,0 +1,31 @@
+require "spec"
+require "../src/knight"
+
+describe "Tools" do
+  it "Location" do
+    a = Location.new(2, 3)
+    a.x.should eq 2
+    a.y.should eq 3
+    a.to_s.should eq "(B, 3)"
+
+    b = Location.new('A', 4)
+    b.x.should eq 1
+    b.y.should eq 4
+    b.to_s.should eq "(A, 4)"
+  end
+
+  it "on_board?" do
+    brd = Board.new(8, 8)
+    brd.on_board?(Location.new('A', 0)).should be_false
+    brd.on_board?(Location.new('A', 1)).should be_true
+    brd.on_board?(Location.new('A', 3)).should be_true
+    brd.on_board?(Location.new('A', 8)).should be_true
+    brd.on_board?(Location.new('A', 9)).should be_false
+    brd.on_board?(Location.new('H', 1)).should be_true
+    brd.on_board?(Location.new('H', 8)).should be_true
+    brd.on_board?(Location.new('I', 1)).should be_false
+
+    #mvs = moves({'A', 3}, {8, 8})
+    #mvs.empty?.should be_true
+  end
+end
---
+++ examples/crystal/adventure_of_knight/src/knight.cr
@@ -0,0 +1,107 @@
+# Have a way to define the location of the treasures
+# Regular Knight's tour:
+# In the path for each step keep the coordinates and a number indication the location in the 8 possible movies from the previous move.
+# The next step is
+#    if the length of the path equals to the number of squares then we are done.
+#       If we would like to find all the solutions then save this solution and go on
+#       else quit
+#    the first empty location starting from the last part of the path and trying to go over the 8 possible locations.
+#       if none of the 8 are possible and empty then this is a dead-end. Replace the last part with the next location.
+#  Given an already existing path the next move is
+#     - if we are in the last to add a new square
+
+# recursive:
+#  given a path and a list oftreasures to find see if we go over the 8 possible next move, 
+
+class Location
+  @x : Int32
+  @y : Int32
+  getter x
+  getter y
+
+  def initialize(x : Int32, y : Int32)
+    @x = x
+    @y = y
+  end
+
+  def initialize(x : Char, y : Int32)
+    @x = x.ord - 64
+    @y = y
+  end
+
+  def to_s
+    return "(#{(@x+64).chr}, #{@y})"
+  end
+end
+
+class Board
+  @x : Int32
+  @y : Int32
+  def initialize(x : Int32, y : Int32)
+   @x = x
+   @y = y
+  end
+
+  def on_board?(location : Location)
+    return false if location.x <= 0
+    return false if location.y <= 0
+    return false if location.x > @x
+    return false if location.y > @y
+    return true
+  end
+
+end
+
+
+#treasures = [
+#    {"e", 6},
+#    {"c", 4},
+#]
+start_here = {"a", 8}
+#p! typeof(start)     # Tuple(String, Int32)
+#p! typeof(treasures) # Array(Tuple(String, Int32))
+
+def moves(location, board)
+  x = location[0].bytes[0]-64
+  y = location[1]
+  return [] of String
+end
+
+
+class Knight
+  @path = [] of Tuple(String, Int32)
+  @current : Tuple(String, Int32)
+
+  def initialize(start)
+    @current = start
+  end
+
+  def find()
+    while (! done?)
+      move = next_move
+      if move.nil?
+        @path.pop
+      else
+        @path.push(move)
+      end
+    end
+  end
+
+  def next_move
+    @current
+    return {"b", 3}
+  end
+
+  def done?
+    return true
+  end
+
+  def path
+    return @path
+  end
+end
+
+kn = Knight.new(start_here)
+kn.find
+puts kn.path
+
