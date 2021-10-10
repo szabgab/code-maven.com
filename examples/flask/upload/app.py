683e231ea063b8cd4b1f137069df7ac0a6475e5c
Gabor Szabo <gabor@szabgab.com>
2021-10-10 11:45:43 +0300

    flask upload example

---
+++ examples/flask/upload/app.py
@@ -0,0 +1,64 @@
+import json
+import os
+import uuid
+from flask import Flask, flash, request, redirect, url_for, render_template, send_file
+from werkzeug.utils import secure_filename
+
+# the "files" directory next to the app.py file
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'files')
+#print(UPLOAD_FOLDER)
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = 'Sick Rat'
+
+@app.route('/', methods=['GET'])
+def main_page():
+    return _show_page()
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file:
+        original_filename = file.filename
+        extension = original_filename.rsplit('.', 1)[1].lower()
+        filename = str(uuid.uuid1()) + '.' + extension
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        file_list = os.path.join(UPLOAD_FOLDER, 'files.json')
+        files = _get_files()
+        files[filename] = original_filename
+        with open(file_list, 'w') as fh:
+            json.dump(files, fh)
+        flash('Upload succeeded')
+        return redirect(url_for('upload_file'))
+    return render_template('upload.html')
+
+
+@app.route('/download/<code>', methods=['GET'])
+def download(code):
+    files = _get_files()
+    if code in files:
+        path = os.path.join(UPLOAD_FOLDER, code)
+        if os.path.exists(path):
+            return send_file(path)
+    abort(404)
+
+def _show_page():
+    files = _get_files()
+    return render_template('upload.html', files=files)
+
+def _get_files():
+    file_list = os.path.join(UPLOAD_FOLDER, 'files.json')
+    if os.path.exists(file_list):
+        with open(file_list) as fh:
+            return json.load(fh)
+    return {}
+
