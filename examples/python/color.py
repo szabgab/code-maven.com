8475b65732041bedbea0a584b00989909ff81e1c
Gabor Szabo <gabor@szabgab.com>
2022-09-04 09:52:09 +0300

    publish

---
+++ examples/python/color.py
@@ -0,0 +1,17 @@
+black   = "\033[0;30m"
+red     = "\033[0;31m"
+green   = "\033[0;32m"
+yellow  = "\033[0;33m"
+white   = "\033[0;37m"
+nocolor = "\033[0m"
+
+print("Plain text in the default color")
+print(green)
+print("Green text")
+print(red)
+print("Red text")
+print(f"{yellow} yellow {green} green {red} red")
+print(white)
+print("White text")
+print(black)
+print("Black text")
