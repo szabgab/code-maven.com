04ed620e95656fe56bc8ee6685443efb48bb7a7e
Gabor Szabo <gabor@szabgab.com>
2023-04-27 08:08:23 +0300

    test all the test cases

--- bin/test_txt2md.py
+++ bin/test_txt2md.py
@@ -1,21 +1,16 @@
 import os
 import pytest
 
-articles = [
-    'sites/en/pages/vanilla-javascript-counter.txt',
-    'sites/en/pages/ci-for-perl-data-alias.txt',
-]
+articles = [filename for filename in os.listdir("md") if filename.endswith(".txt")]
 
 @pytest.mark.parametrize("article", articles)
 def test_articles(article, tmpdir):
-    basename = os.path.basename(article)
-    assert os.system(f'./bin/txt2md.py {article} > {tmpdir}/{basename}') == 0
-    assert os.system(f'diff md/{basename} {tmpdir}/{basename}') == 0
+    assert os.system(f'./bin/txt2md.py sites/en/pages/{article} > {tmpdir}/{article}') == 0
+    assert os.system(f'diff md/{article} {tmpdir}/{article}') == 0
     #print(article)
 
 
 if __name__ == '__main__':
     print('generate')
     for article in articles:
-        basename = os.path.basename(article)
-        os.system(f'./bin/txt2md.py {article} > md/{basename}')
+        os.system(f'./bin/txt2md.py sites/en/pages/{article} > md/{article}')
