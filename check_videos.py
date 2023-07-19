37aed16c50eafbfd2d5a638a1db0eab080ff2dc9
Gabor Szabo <gabor@szabgab.com>
2023-07-19 18:03:52 +0300

    update check videos

--- check_videos.py
+++ check_videos.py
@@ -6,6 +6,8 @@ import sys
 # TBD fetch all the videos in the youtube channel
 # For now we can download files via https://takeout.google.com/
 
+# Apparently not all the playlists are inclided in the first csv file in the download
+
 # list the videos that are in none of the playlists
 # check if all the videos on youtube have a page in code-maven.
 
@@ -46,16 +48,44 @@ def get_videos_from_youtube():
                 ids.append(vid)
     return ids
 
+def read_playlists():
+    playlists = {}
+    videos = set()
+    for playlist_file in os.listdir('playlists'):
+        name = playlist_file[0:-4]
+        #print(name)
+        playlists[name] = []
+
+        with open(os.path.join('playlists', playlist_file)) as fh:
+            in_data = False
+            for row in fh:
+                if row.startswith("Video Id,Time Added"):
+                    in_data = True
+                    continue
+                if in_data:
+                    if re.search(r'\S', row):
+                        (vid, _) = row.strip().split(",", 2)
+                        # print(vid)
+                        playlists[name].append(vid)
+                        videos.add(vid)
+    return playlists, videos
+
 def main():
     language = 'he'
     code_maven = set(get_videos_from_code_maven(language))
     youtube = set(get_videos_from_youtube())
+    playlists, videos = read_playlists()
 
     print(sorted(code_maven - youtube))
     # This is strange it shows that we have videos on the site that are not in the YouTube channel.
     # I checked the first one and it was on YouTube as well so it unclear why does this say
 
+    print("Videos on YouTube but not on the site")
     print(sorted(youtube - code_maven))
+
+    print("Videos not in any playlist")
+    print(sorted(youtube - videos))
+
 main()
 
 
