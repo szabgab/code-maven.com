22336d85e9afc984aae309fe2a1d4562e82ba4fd
Gabor Szabo <gabor@szabgab.com>
2025-08-03 09:28:09 +0300

    update colors

--- examples/js/svg_flag_of_hungary.js
+++ examples/js/svg_flag_of_hungary.js
@@ -4,9 +4,9 @@ function flag_of_hungary() {
     var height = width/2;
     draw.size(width, height);
 
-    var red   = draw.rect(width, height/3).fill({ color: '#CD2A3E' });
+    var red   = draw.rect(width, height/3).fill({ color: '#CE2939' });
     var white = draw.rect(width, height/3).fill({ color: '#FFFFFF' }).dy(height/3);
-    var green = draw.rect(width, height/3).fill({ color: '#436F4D' }).dy(2*height/3);
+    var green = draw.rect(width, height/3).fill({ color: '#477050' }).dy(2*height/3);
 }
 flag_of_hungary();
 
