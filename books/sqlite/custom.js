a8f210eb3b17ce656d74a3d222029094337a308b
Gabor Szabo <gabor@szabgab.com>
2026-04-27 07:56:35 +0300

    show filenames

---
+++ books/sqlite/custom.js
@@ -0,0 +1,4 @@
+const prefixes = ["src/examples/"];
+const extensions = [".py", ".toml", ".cfg", ".sql"];
+const base_url = "https://github.com/szabgab/python.code-maven.com/tree/main/books/python-testing/"
+
